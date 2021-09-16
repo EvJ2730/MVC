e1325b8ac4cc3ecc71865eefc62f1149f3799551
EvJ <73695353+EvJ2730@users.noreply.github.com>
2021-09-16 14:57:41 +0200

    changes

--- MusicApp/MusicApp.Domain/BaseEntity.cs
+++ MusicApp/MusicApp.Domain/BaseEntity.cs
@@ -1,10 +1,12 @@
-﻿using System;
+﻿using MusicApp.Domain.Models;
+using System;
 
 namespace MusicApp.Domain
 {
     public class BaseEntity
     {
         public int Id { get; set; }
+        public User User { get; set; }
         public DateTime CreatedOn { get; set; }
         public DateTime UpdatedOn { get; set; }
     }
